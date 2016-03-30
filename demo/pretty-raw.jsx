0a171b5b24a9b34b15f98f187fee1bb5ce2fac25
Paul Frazee <pfrazee@gmail.com>
2016-03-30 15:26:03 -0500

    fix bug in demo

--- demo/pretty-raw.jsx
+++ demo/pretty-raw.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as PrettyRaw from '../pretty-raw'
-import msgs from '../fixtures/msgs'
+import { posts } from '../fixtures/msgs'
 
 export default class PrettyRawView extends React.Component {
   render() {
@@ -8,13 +8,13 @@ export default class PrettyRawView extends React.Component {
       <section className="prettyraw-table">
         <header>&lt;PrettyRaw.Table&gt;</header>
         <div className="content">
-          <div className="example"><PrettyRaw.Table obj={msgs[0]} /></div>
+          <div className="example"><PrettyRaw.Table obj={posts[0]} /></div>
         </div>
       </section>
       <section className="prettyraw-div">
         <header>&lt;PrettyRaw.Div&gt;</header>
         <div className="content">
-          <div className="example"><PrettyRaw.Div obj={msgs[0]} /></div>
+          <div className="example"><PrettyRaw.Div obj={posts[0]} /></div>
         </div>
       </section>
     </div>
